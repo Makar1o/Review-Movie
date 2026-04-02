ab44137fdda69491faf15b86e4ca3c5bd50c6d2e
Makar1o <ruslan.mykytyn.03@gmail.com>
2026-04-03 01:43:12 +0300

    check how works active days

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {/* comment */}
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
